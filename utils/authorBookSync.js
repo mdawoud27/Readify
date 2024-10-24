600207b3bbe891a470794c210f0e0086c957cb0f
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-10-24 11:14:31 +0300

    chore: clean up `authorBookSync.js` by removing unnecessary comments

--- utils/authorBookSync.js
+++ utils/authorBookSync.js
@@ -8,16 +8,13 @@ async function updateAuthorBooks() {
   try {
     const books = await Book.find();
 
-    // Using Promise.all to handle async operations within the loop
     const updatePromises = books.map(async (book) => {
       return await Author.findByIdAndUpdate(book.author, {
         $addToSet: { books: book._id },
       });
     });
 
-    // Wait for all updates to complete
     await Promise.all(updatePromises);
-
     console.log("Authors updated with corresponding books!");
   } catch (error) {
     console.error("Error updating authors:", error.message);
@@ -29,26 +26,21 @@ async function removeBooksFromOldAuthors() {
   try {
     const books = await Book.find();
 
-    // Loop through each book
     const removePromises = books.map(async (book) => {
-      // Find all authors except the current one who might still have this book
       const oldAuthors = await Author.find({
         books: book._id,
         _id: { $ne: book.author },
       });
 
-      // Remove the book from all old authors
       const updateOldAuthorsPromises = oldAuthors.map(async (oldAuthor) => {
         return await Author.findByIdAndUpdate(oldAuthor._id, {
           $pull: { books: book._id },
         });
       });
 
-      // Wait for all updates to complete
       return Promise.all(updateOldAuthorsPromises);
     });
 
-    // Wait for all authors to be updated
     await Promise.all(removePromises);
 
     console.log("Books removed from old authors successfully!");
